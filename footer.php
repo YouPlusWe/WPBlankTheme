9edd4e5eb374403ccd3baf4f270c1c6e16435170
Nikola Nikolitsov <nikola.nikolitsov@youpluswe.com>
2021-05-18 14:05:41 +0300

    remove jquery

--- footer.php
+++ footer.php
@@ -10,8 +10,7 @@
 	</footer>
 
 	<?php wp_footer(); ?>
-
-	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	
 	<script async src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
 
 </body>
